83d9d3b80727fe8af6c48f150a24f09b07480afe
DannyJimenezA <anjiar03@hotmail.com>
2025-09-16 16:46:10 -0600

    Cambios dockerfile, para prueba de despliegue en render

--- app/Http/Controllers/Api/catalogo/HabitacionAmenidadController.php
+++ app/Http/Controllers/Api/catalogo/HabitacionAmenidadController.php
@@ -2,8 +2,8 @@
 namespace App\Http\Controllers\Api\catalogo;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreHabitacionAmenidadRequest;
-use App\Models\HabitacionAmenidad;
+use App\Http\Requests\catalogo\StoreHabitacionAmenidadRequest;
+use App\Models\habitacion\HabitacionAmenidad;
 
 class HabitacionAmenidadController extends Controller
 {
