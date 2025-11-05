55376e340b15671022c8913a92c1d6ef98a88775
arielcamposchavarria <campos2004.05@gmail.com>
2025-11-05 12:35:26 -0600

    chanche-datetime/null

---
+++ database/migrations/updates/2025_11_05_120739_update_fechas_to_datetime_in_limpiezas_and_mantenimientos_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('limpiezas', function (Blueprint $table) {
+            $table->dateTime('fecha_inicio')->change();
+            $table->dateTime('fecha_final')->nullable()->change();
+        });
+
+        Schema::table('mantenimientos', function (Blueprint $table) {
+            $table->dateTime('fecha_inicio')->nullable()->change();
+            $table->dateTime('fecha_final')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('limpiezas', function (Blueprint $table) {
+            $table->date('fecha_inicio')->change();
+            $table->date('fecha_final')->nullable()->change();
+        });
+
+        Schema::table('mantenimientos', function (Blueprint $table) {
+            $table->date('fecha_inicio')->nullable()->change();
+            $table->date('fecha_final')->nullable()->change();
+        });
+    }
+};
