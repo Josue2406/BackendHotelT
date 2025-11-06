f517d3fe1d6c3d23fb479e4d8f92b37f8ecfc61e
DannyJimenezA <anjiar03@hotmail.com>
2025-11-05 22:54:25 -0600

    Cors

--- config/cors.php
+++ config/cors.php
@@ -27,5 +27,5 @@ return [
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,         // déjalo en false si NO envías cookies
+    'supports_credentials' => true,         // déjalo en false si NO envías cookies
 ];
