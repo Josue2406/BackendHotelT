ab66bc114f13bdbdac205a6360054c2f9a473c0c
Jose Guadamuz Elizondo <jose.guadamuz.elizondo@est.una.ac.cr>
2025-11-11 09:52:11 -0600

    Update CORS configuration to specify allowed origins for enhanced security

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     // Lee los orígenes permitidos desde .env
     // IMPORTANTE: No se puede usar '*' con credentials:true
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '')))),
+    'allowed_origins' => ['https://lanaku-hotel.onrender.com'],
 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
